090257bb7bf7325b479fa6347d8706f87e42ad32
Marc Tuxing Machine <mkrabsde@gmail.com>
2024-01-16 13:55:15 +0100

    [feat] Class skeletons

--- AutoLike.js
+++ AutoLike.js
@@ -6,4 +6,68 @@
 // Firefox Add-on: https://addons.mozilla.org/en-US/firefox/addon/autolike/
 
 
+class AutoLikeAddon {
+    constructor() {
+        this.prefix = "[AutoLike]";
+        this.syncStorage = new SyncManager();
+        this.videoManager = new VideoManager();
+        this.channelManager = new ChannelManager();
+        this.init();
+    }
 
+    init() {
+        console.debug(`${this.prefix} Initializing...`);
+        this.syncStorage.getStorageValues();
+    }
+
+}
+
+class SyncManager {
+    constructor() {
+        this.processedVideoIds = [];
+        this.channelIds = [];
+        this.getStorageValues();
+    }
+
+    getStorageValues() {
+    }
+
+    setStorageValues() {
+    }
+}
+
+class VideoManager {
+    constructor() {
+        // Add any necessary properties here
+    }
+
+    watchForMouseHover() {
+        // Implementation here
+    }
+
+    likeVideoIfAsked() {
+        // Implementation here
+    }
+
+    addToBlacklist() {
+        // Implementation here
+    }
+}
+
+class ChannelManager {
+    constructor() {
+        // Add any necessary properties here
+    }
+
+    findCurrentChannel() {
+        // Implementation here
+    }
+
+    channelIncluded() {
+        // Implementation here
+    }
+}
+
+// Instantiate the addon
+const autoLikeAddon = new AutoLikeAddon();
+autoLikeAddon.run();
